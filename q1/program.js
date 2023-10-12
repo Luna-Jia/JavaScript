30bcd3125062bb3f668582d64c9cc063cf311fac
Meng Luna Jia <73403516+Luna-Jia@users.noreply.github.com>
2023-10-12 10:41:44 -0400

    q13: regular expression

--- q1/program.js
+++ q1/program.js
@@ -184,4 +184,24 @@ function f(n){
 
 // console.log(ApplyFunctionToArray(f,[4,8,36,7]));
 
+/* 13.
+Write a function named university that takes a string as a parameter and decides whether it is a valid 
+university id. The university id format is E-0xxy-9yyx. Write a method named phone that takes a string as a 
+parameter and decides whether it is a valid phone number with area code 313 or 248 or 734. The phone number 
+format is xxx-xxx-xxxx. Here x is a digit and y is a lower case letter. These functions return true/false. The 
+code must be based on regular expressions.
+*/
 
+function university(id) {
+    let reg = /^E-0\d{2}[a-z]-9[a-z]{2}\d$/;
+    return reg.test(id);
+}
+
+function phone(number) {
+    let reg = /^(313|248|734)-\d{3}-\d{4}$/;
+    return reg.test(number);
+}
+
+// console.log(university("E-045a-9cdd"));
+
+// console.log(phone("313-254-6983"));
