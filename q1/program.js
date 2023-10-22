b52d2fc2f334f1bc51bbe53c422a9fa04658818a
Meng Luna Jia <73403516+Luna-Jia@users.noreply.github.com>
2023-10-22 11:25:21 -0400

    add even number of questions, complete q1

--- q1/program.js
+++ q1/program.js
@@ -70,6 +70,37 @@ function tax(income,status){
 
 // console.log(tax(355000,"single"))
 
+// #4
+/* Write a function named stdDev that computes the standard deviation of a set of numbers. 
+ The function takes the numbers as a variable number of parameters and returns the standard 
+ deviation. The standard deviation of n numbers x1, x2 . . . xn is the square root of 
+ [((x1-m)2 + (x2-m)2 + . . . + (xn-m)2)/n] where m is the average of the 
+ numbers. If there are less than two numbers the standard deviation is 0.
+ */
+
+ function stdDev()
+ {
+     // calculate average of numbers
+     let sum = 0
+     for (var i = 0; i < arguments.length; i++)
+     {
+         sum += arguments[i]
+     }
+     const average = sum / arguments.length
+     
+     // calculate standard deviation
+     let stdDeviation = 0
+     for (var i = 0; i < arguments.length; i++)
+     {
+         stdDeviation += Math.pow(arguments[i] - average, 2)
+     }
+     stdDeviation /= arguments.length
+     stdDeviation = Math.sqrt(stdDeviation)
+     
+     return stdDeviation
+ }
+ 
+
 /* 5
 Write a function named compoundInterest that computes the compound interest. If p amount is invested for 
 n years with interest rate r and the money is compounded annually then the final amount will be 
@@ -84,6 +115,21 @@ function compoundInterest(p, r, n){
 }
 
 // console.log(compoundInterest(5000, 7,20));
+
+//#6
+/* Write a function that takes a character as a parameter and returns its type. The possible types 
+ are upper, lower, digit, and other. Upper means an upper case letter. Lower means a 
+ lower case letter. Digit means a numerical digit. Other means all other characters. The return 
+ value is a string in lower case. Regular expressions cannot be used.
+ */
+
+ function checkType(ch)
+ {
+     if("0" <= ch && ch <= "9") return "digit"	// digit
+     else if("a" <= ch && ch <= "z") return "lower"	// lowercase
+     else if("A" <= ch && ch <= "Z") return "upper"	// uppercase
+     else return "other"				// special character
+ }
 
 /* 7.
 Write a function named createIdPassword that takes a last name and a first name as parameters and returns 
@@ -112,6 +158,43 @@ function createIdPassword(lname, fname) {
 }
 
 // console.log(createIdPassword("Jia", "Luna"));
+
+// #8
+/* Write a function named removeDuplicates that takes an array of strings as parameter and 
+ returns a duplicate free array. The calling/parameter array does not change. The unique strings of
+ the calling array are placed in a newly created array and it is returned. For example if the calling 
+ array is [tree, cat, box, cat, dog, tree, tree] then the returned array is 
+ [tree, cat, box, dog].
+ */
+
+ function removeDuplicates(inArray)
+ {
+     // declare outArray to return later
+     let outArray = []
+     
+     // for every value in inArray, add it to outArray if not in outArray yet
+     for(let inVal of inArray)
+     {
+         let isDuplicate = false
+ 
+         for (let outVal of outArray)
+         {
+             // determine whether value in inArray is already in outArray
+             if(inVal == outVal)
+             {
+                 isDuplicate = true
+                 break
+             }
+         }
+         // push value from inArray to outArray if it is unique
+         if(!isDuplicate)
+         {
+             outArray.push(inVal)
+         }
+     }
+     
+     return outArray
+ }
 
 /* 9.
 Write a function named mySort that takes three arrays and sort/rearrange them in parallel. The three 
@@ -161,6 +244,52 @@ console.log(gpas);     // [3.0, 4.0, 3.5]
 console.log(zips); // ['11111', '67890', '12345']
 */
 
+// #10
+/* Write a function named myReverse that takes a line of words as a paramter, reverses the 
+order of words, reverses the order of characters in every other word, and returns the result. The 
+input and the output are strings. The words are separated by single spaces in the input and the 
+output. For example if the line of words is tree is big green then the result is neerg 
+big si tree
+*/
+
+function myReverse(line)
+{
+	// convert to array
+	line = line.split(" ")
+
+	// reverse order of words
+	for(let i = 0; i < Math.floor(line.length / 2); i++)
+	{
+		let temp = line[i]
+		line[i] = line[line.length - 1 - i]
+		line[line.length - 1 - i] = temp
+	}
+
+	// reverse order of characters in every other word
+	for(let i = 0; i < line.length; i++)
+	{
+		if(i % 2 == 0) // only every other word
+		{
+			let temp = ""
+			for(let j = line[i].length - 1; j >= 0; j--)
+			{
+				temp += line[i].charAt(j)
+			}
+			line[i] = temp
+		}
+	}
+
+	// convert back to string
+	let outLine = ""
+	for(let i = 0; i < line.length; i++)
+	{
+		outLine += line[i] + " "
+	}
+	outLine = outLine.trim()
+	return outLine
+}
+
+
 
 /* 11. 
 Write a function named ApplyFunctionToArray that takes a function f and an array p as parameters. The f is 
@@ -183,6 +312,56 @@ function f(n){
 }
 
 // console.log(ApplyFunctionToArray(f,[4,8,36,7]));
+
+// #12
+/* Write a class called Student. The class has two properties called name and gpa. The class 
+has a constructor that takes a name and a gpa and set them to the properties of the class. The 
+class getName, getGpa, setName, and setGpa methods that get and set name and gpa. The 
+class has isHonors method which returns true/false depending on whether gpa is above 
+or below 3. The class has toString method that returns a string containing name and gpa
+*/
+
+class Student
+{
+	// constructor
+	constructor(name, gpa)
+	{
+		this.name = name 
+		this.age = age
+	}
+	
+	// getters
+	getName()
+	{
+		return this.name
+	}	
+	getGpa()
+	{
+		return this.gpa
+	}
+	
+	// setters
+	setName(name)
+	{
+		this.name = name
+	}
+	setGpa(gpa)
+	{
+		this.gpa = gpa
+	}
+	
+	// says whether student is honors or no
+	isHonors()
+	{
+		return gpa > 3
+	}
+
+	// toString, print 
+	toString()
+	{
+		return "Name: " + name + "\tGPA: " + gpa + "\t"
+	}
+}
 
 /* 13.
 Write a function named university that takes a string as a parameter and decides whether it is a valid 
@@ -211,3 +390,18 @@ function phone(number) {
 // console.log(university("E-045a-9cdd"));
 
 // console.log(phone("313-254-6983"));
+
+// #14
+/* Write a function named fullName that takes a string as a parameter and decides whether it 
+ is a full name of a person. The full name format is Prefix First M. Last. Prefix is Mr, 
+ Mrs, or Ms. First is the first name that begins in upper case letter followed by one or more lower 
+ case letters with a total length of at least two. M is the middle initial which is a single upper case 
+ letter. Last is the last name which has the same format as the first name. There is a dot after the 
+ middle initial. There is one space between prefix, first name, middle initial, and last name. The
+ function returns true/false. The code must be based on regular expressions
+ */
+
+ function fullName(name)
+ {
+     return name.search(/^Mr?s? [A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+$/) == 0 // returns true if name matches that format, false otherwise
+ }
